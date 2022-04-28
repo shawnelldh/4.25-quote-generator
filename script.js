43e15bc74ed6ae1594932c5da600396a5f927ea5
shawnelldh <shawnelldh@gmail.com>
2022-04-28 15:42:39 -0400

    1 file modified

--- script.js
+++ script.js
@@ -19,10 +19,42 @@ const loader = document.getElementById('loader');
        }
  }
 
+// Get Quote From API
+async function getQuote() {
+   loading();
+   const proxyUrl = 'https://whispering-tor-04671.herokuapp.com/'
+   const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
+   try {
+       const response = await fetch(proxyUrl + apiUrl);
+       const data = await response.json();
+       // If Author is blank, add 'Unknown'
+       if (data.quoteAuthor === '') {
+           authorText.innerText = 'Unknown';
+       } else {
+           authorText.innerText = data.quoteAuthor;
+       }
+       // Reduce font size for long quotes
+       if (data.quoteText.length > 120) {
+           quoteText.classList.add('long-quote');
+       } else {
+           quoteText.classList.remove('long-quote');
+       }
+    quoteText.innerText = data.quoteText;
+      //  Stop Loader, Show Quote\\
+      complete();
+      } catch (error) {
+         getQuote();
+      }
 
-    
+   }
 
+   // Tweet Quote
+   function tweetQuote() {
+      const quote = quoteText.innerText;
+      const author = authorText.innerText;
+      
 
+   }
 
-
+   
   
