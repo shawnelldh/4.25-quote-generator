ae58ca87814aaa3329886f798e90e866a5c434fa
shawnelldh <shawnelldh@gmail.com>
2022-05-08 18:15:11 -0400

    3 files modified

---
+++ script.js
@@ -0,0 +1,24 @@
+const quoteContainer = document.getElementById("quote-container");
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
+
+// Show Loading
+function loading() {
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+// Hide Loading
+function complete() {
+    if (!loader.hidden) {
+       quoteContainer.hidden = false;
+       loader.hidden = true;
+
+
+    }
+
+
+}
